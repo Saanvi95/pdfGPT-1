206f5173d21d7fe478ef103a9d96f2876aca8051
Saanvi95 <130914458+Saanvi95@users.noreply.github.com>
2023-05-11 09:27:15 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,8 @@
 import json
 from tempfile import _TemporaryFileWrapper
 
-import streamlit as st
 import requests
-
+import streamlit as st
 
 def ask_api(
     lcserve_host: str,
@@ -50,45 +49,38 @@ def ask_api(
 
     return r.json()['result']
 
+def main():
+    st.set_page_config(page_title="PDF GPT", page_icon="📚")
 
-title = 'PDF GPT'
-description = """ PDF GPT allows you to chat with your PDF file using Universal Sentence Encoder and Open AI. It gives hallucination free response than other tools as the embeddings are better than OpenAI. The returned response can even cite the page number in square brackets([]) where the information is located, adding credibility to the responses and helping to locate pertinent information quickly."""
+    title = 'PDF GPT'
+    description = """ PDF GPT allows you to chat with your PDF file using Universal Sentence Encoder and Open AI. It gives hallucination free response than other tools as the embeddings are better than OpenAI. The returned response can even cite the page number in square brackets([]) where the information is located, adding credibility to the responses and helping to locate pertinent information quickly."""
 
-with st.Blocks() as demo:
-    st.Markdown(f'<center><h1>{title}</h1></center>')
-    st.Markdown(description)
+    st.markdown(f'<center><h1>{title}</h1></center>', unsafe_allow_html=True)
+    st.markdown(description)
 
-    with st.Row():
-        with st.Group():
-            lcserve_host = st.Textbox(
-                label='Enter your API Host here',
-                value='http://localhost:8080',
-                placeholder='http://localhost:8080',
-            )
-            st.Markdown(
-                f'<p style="text-align:center">Get your Open AI API key <a href="https://platform.openai.com/account/api-keys">here</a></p>'
-            )
-            openAI_key = st.Textbox(
-                label='Enter your OpenAI API key here', type='password'
-            )
-            pdf_url = st.Textbox(label='Enter PDF URL here')
-            st.Markdown("<center><h4>OR<h4></center>")
-            file = st.File(
-                label='Upload your PDF/ Research Paper / Book here', file_types=['.pdf']
-            )
-            question = st.Textbox(label='Enter your question here')
-            btn = st.Button(value='Submit')
-            btn.style(full_width=True)
+    lcserve_host = st.text_input(
+        'Enter your API Host here',
+        value='http://localhost:8080',
+    )
+    openAI_key = st.text_input(
+        'Enter your OpenAI API key here',
+        type='password',
+        help='Get your Open AI API key here: https://platform.openai.com/account/api-keys',
+    )
+    pdf_url = st.text_input('Enter PDF URL here')
+    file = st.file_uploader(
+        'Upload your PDF/ Research Paper / Book here',
+        type=['pdf'],
+    )
+    question = st.text_input('Enter your question here')
+    submit = st.button('Submit')
 
-        with st.Group():
-            answer = st.Textbox(label='The answer to your question is :')
+    if submit:
+        try:
+            answer = ask_api(lcserve_host, pdf_url, file, question, openAI_key)
+            st.success(f'The answer to your question is: {answer}')
+        except ValueError as e:
+            st.error(str(e))
 
-        btn.click(
-            ask_api,
-            inputs=[lcserve_host, pdf_url, file, question, openAI_key],
-            outputs=[answer],
-        )
-
-demo.app.server.timeout = 60000 # Set the maximum return time for the results of accessing the upstream server
-
-demo.launch(server_port=7860, enable_queue=True) # `enable_queue=True` to ensure the validity of multi-user requests
+if __name__ == '__main__':
+    main()
